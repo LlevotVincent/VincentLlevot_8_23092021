069ff553fe240f00fd476ca9247f680e7f105d3b
LlevotVincent <vincent.llevot@gmail.com>
2021-09-28 16:00:48 +0200

    netoyage

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import './index.css';
 import Home from './Pages/Home';
 import Logement from './Pages/Logement';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from '../src/Components/Header'
 
 
 ReactDOM.render(
   <React.StrictMode>
       <Router>
-          <Header />
-        <Route path="./pages/Home">
+          {/* <Header /> */}
+        <Route path="./Pages/Home">
           <Home />
         </Route>
         <Route path="./Pages/Logement">
