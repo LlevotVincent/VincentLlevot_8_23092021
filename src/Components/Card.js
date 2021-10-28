39cd9695e50912a7c11268cbab04db2dc3125d21
LlevotVincent <vincent.llevot@gmail.com>
2021-10-28 12:24:38 +0200

    mise a jour CSS

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -8,7 +8,7 @@ class Card extends React.Component {
         return (
             <Link to={`/Logement/${this.props.logement.id}`}>
                 <div className='Card'>
-                    <img className='Card-background' src={this.props.logement.cover} alt='appartement' />
+                    <img className='Card-img' src={this.props.logement.cover} alt='appartement' />
                     <h2>{this.props.logement.title}</h2>
                 </div>
             </Link>
